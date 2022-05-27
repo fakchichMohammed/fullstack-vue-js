fe67f83865b02b10be0487a82440f0c887fdadec
Mohammed FAKCHICH <mohammed_fakchich@carrefour.com>
2022-05-27 10:48:02 +0200

    Create an express server and configure all dependencies

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const PORT = process.env.PORT || 4000;
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./config/db");
+const app = express();
+
+// configure mongoose and database
+mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(config.database, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log({ database_error: err });
+  });
+// db configuration ends here
+
+// registering cors
+app.use(cors());
+
+// configure body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// configure body-parser ends here
+
+// configure morgan
+app.use(morgan("dev"));
+// configure morgan ends here
+
+// define first route
+app.get("/", (req, res) => {
+  console.log("Hello mevn developer");
+});
+// bring in our user routes
+const useRoutes = require("./api/user/route/user");
+app.use("/user", useRoutes);
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
